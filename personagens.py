1812455e70a48e6bc37142482a424b10544e3cbb
Pedro Felipe Arruda <pedrofelipega@gmail.com>
2022-07-06 14:54:14 -0400

    Troca de atributos do perosnagem e criação de batalha propria

--- personagens.py
+++ personagens.py
@@ -1,49 +1,69 @@
 from faker import Faker # Os dados fake para nome
 import random
 
-fake = Faker() 
-  
-class Guerreiro:
-  def __init__(self, nome = fake.first_name()):
+fake = Faker()
+
+class Personagem:
+  def __init__(self, nome):
     self.nome = nome
+    self.bag = []
+  
+class Guerreiro(Personagem):
+  def __init__(self):
+    super().__init__(fake.first_name())
     self.classe = 'Guerreiro'
-    self.vida = random.randint(4,6)
+    self.forca = random.randint(8,10)
+    self.agilidade = random.randint(4,6)
+    self.inteligencia = random.randint(0,1)
+    self.vida = int(self.forca*1)
+    self.ataque = int(self.forca/2)
+    self.defesa = int(self.agilidade/2)
+    self.mana = int(self.inteligencia*2)
     self.vidaTotal = self.vida
-    self.ataque = random.randint(1,3)
     self.dano = 0
     self.range = 1
-    self.bag = []
+    
 
-class Arqueiro:
-  def __init__(self, nome = fake.first_name()):
-    self.nome = nome
+class Arqueiro(Personagem):
+  def __init__(self):
+    super().__init__(fake.first_name())
     self.classe = 'Arqueiro'
-    self.vida = random.randint(2,4)
+    self.forca = random.randint(4,6)
+    self.agilidade = random.randint(8,10)
+    self.inteligencia = random.randint(0,2)
+    self.vida = int(self.forca*1)
+    self.ataque = int(self.agilidade/2)
+    self.defesa = int(self.agilidade/2)
+    self.mana = int(self.inteligencia*2)
     self.vidaTotal = self.vida
-    self.ataque = random.randint(1,2)
     self.dano = 0
     self.range = 2
-    self.bag = []
+    
 
-class Mago:
-  def __init__(self, nome = fake.first_name()):
-    self.nome = nome
+class Mago(Personagem):
+  def __init__(self):
+    super().__init__(fake.first_name())
     self.classe = 'Mago'
-    self.vida = random.randint(1,3)
+    self.forca = random.randint(2,4)
+    self.agilidade = random.randint(2,4)
+    self.inteligencia = random.randint(10,12)
+    self.vida = int(self.forca*1)
+    self.ataque = int(self.forca/2)
+    self.defesa = int(self.agilidade/2)
+    self.mana = int(self.inteligencia*2)
     self.vidaTotal = self.vida
-    self.ataque = 1
     self.dano = 0
     self.range = 2
-    self.bag = []
+    
 
 
-class Curandeiro:
-  def __init__(self, nome = fake.first_name()):
-    self.nome = nome
+class Curandeiro(Personagem):
+  def __init__(self):
+    super().__init__(fake.first_name())
     self.classe = 'O Super Senhor Curandeiro'
     self.vida = random.randint(1,2)
     self.vidaTotal = self.vida
     self.ataque = random.randint(0,1)
     self.dano = 0
     self.range = 3
-    self.bag = []
+    

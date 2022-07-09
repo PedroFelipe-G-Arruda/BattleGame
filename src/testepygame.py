0d4ee9ef57db13a499b0efb804c05b66e03df666
Yona Hirakawa <hirakawayona@gmail.com>
2022-07-09 14:49:07 -0300

    Menu and background updates

--- src/testepygame.py
+++ src/testepygame.py
@@ -5,56 +5,60 @@ pygame.init()
 x = 1280
 y = 720
 
+pos_x = x/2
+pos_y = 5*y/12
+
+h = 200
+
 screen = pygame.display.set_mode((x,y))
 pygame.display.set_caption('Joguinho de Batalha')
 
-bg = pygame.image.load('images/background.jpg').convert_alpha()
+bg = pygame.image.load('images/bg.jpg').convert_alpha()
 bg = pygame.transform.scale(bg, (x, y))
 
 warrior = pygame.image.load('images/warrior.png').convert_alpha()
-warrior = pygame.transform.scale(warrior, (150,150))
+warrior = pygame.transform.scale(warrior, (h,h))
 warrior = pygame.transform.flip(warrior,True,False)
 
 warrior_enemy = pygame.image.load('images/warrior-en2.png').convert_alpha()
-warrior_enemy = pygame.transform.scale(warrior_enemy, (150,150))
+warrior_enemy = pygame.transform.scale(warrior_enemy, (h,h))
 
+pos_warrior_x = pos_x - 180
+pos_warrior_y = pos_y + 40
 
-pos_warrior_x = 300
-pos_warrior_y = 530
-
-pos_warrioren_x = 500
-pos_warrioren_y = 530
+pos_warrioren_x = pos_x
+pos_warrioren_y = pos_y + 40
 
 
 
 archer = pygame.image.load('images/rodolfo.png').convert_alpha()
-archer = pygame.transform.scale(archer, (150,150))
+archer = pygame.transform.scale(archer, (h,h))
 archer = pygame.transform.flip(archer,True,False)
 
 archer_enemy = pygame.image.load('images/rodolfo-enemy.png').convert_alpha()
-archer_enemy = pygame.transform.scale(archer_enemy, (150,150))
+archer_enemy = pygame.transform.scale(archer_enemy, (h,h))
 
 
-pos_archer_x = 200
-pos_archer_y = 450
+pos_archer_x = pos_x - 180 * 2
+pos_archer_y = pos_y
 
-pos_archeren_x = 600
-pos_archeren_y = 450
+pos_archeren_x = pos_x + 180
+pos_archeren_y = pos_y
 
 
 mage = pygame.image.load('images/mage.png').convert_alpha()
-mage = pygame.transform.scale(mage, (150,150))
+mage = pygame.transform.scale(mage, (h,h))
 mage = pygame.transform.flip(mage,True,False)
 
 mage_enemy = pygame.image.load('images/mage-en2.png').convert_alpha()
-mage_enemy = pygame.transform.scale(mage_enemy, (150,150))
+mage_enemy = pygame.transform.scale(mage_enemy, (h,h))
 
 
-pos_mage_x = 100
-pos_mage_y = 300
+pos_mage_x = pos_x - 160 * 3
+pos_mage_y = pos_y - 40 
 
-pos_mageen_x = 700
-pos_mageen_y = 300
+pos_mageen_x = pos_x + 150 * 2
+pos_mageen_y = pos_y - 40
 
 rodando = True 
 
@@ -74,6 +78,11 @@ while rodando:
 
     screen.blit(mage, (pos_mage_x, pos_mage_y))
     screen.blit(mage_enemy, (pos_mageen_x, pos_mageen_y))
+
+    #for i in range(0,721,60):
+    #    pygame.draw.line(screen, (255, 0, 0), (0, i), (x, i))
+    #pygame.draw.line(screen, (255, 0, 0), (x/2,0), (x/2, y))
+    #pygame.display.flip()
 
     pygame.display.update()
 
